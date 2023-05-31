f7509d044df3aa26e2144885bf0cca154ca2f5b6
Finn Poduska <111396997+ImThatOneGuyInTheCorner@users.noreply.github.com>
2023-05-31 23:57:29 +0000

    Wed, May 31, 2023, 4:57 PM -07:00

--- js/index.js
+++ js/index.js
@@ -40,19 +40,19 @@ function update() {
     }
 
     if (currentActive == 1) {
-        document.getElementById('Daft-Punk').style.display = 'flex';
-        document.getElementById('Gorillaz').style.display = 'none';
-        document.getElementById('Passion-Pit').style.display = 'none';
+        document.getElementById('Daft-Punk').style.left = '150px';
+        document.getElementById('Gorillaz').style.left = '1500000px';
+        document.getElementById('Passion-Pit').style.left = '1500000px';
     }
     else if (currentActive == 2) {
-        document.getElementById('Daft-Punk').style.display = 'none';
-        document.getElementById('Gorillaz').style.display = 'flex';
-        document.getElementById('Passion-Pit').style.display = 'none';
+        document.getElementById('Daft-Punk').style.left = '1500000px';
+        document.getElementById('Gorillaz').style.left = '150px';
+        document.getElementById('Passion-Pit').style.left = '1500000px';
     }
     else {
-        document.getElementById('Daft-Punk').style.display = 'none';
-        document.getElementById('Gorillaz').style.display = 'none';
-        document.getElementById('Passion-Pit').style.display = 'flex';
+        document.getElementById('Daft-Punk').style.left = '1500000px';
+        document.getElementById('Gorillaz').style.left = '1500000px';
+        document.getElementById('Passion-Pit').style.left = '150px';
     }
 
     circles.forEach((circle, index) => {
